7746959835f874a3878508017a31c2db0d6e8323
Dustin <126348160+Dustin4444@users.noreply.github.com>
2025-08-05 17:10:04 -0400

    Revert "Ensure auto approve runs on rewritten command"

--- src/vs/workbench/contrib/terminalContrib/chatAgentTools/browser/runInTerminalTool.ts
+++ src/vs/workbench/contrib/terminalContrib/chatAgentTools/browser/runInTerminalTool.ts
@@ -194,22 +194,15 @@ export class RunInTerminalTool extends Disposable implements IToolImpl {
 		});
 		const language = os === OperatingSystem.Windows ? 'pwsh' : 'sh';
 
-		const instance = context.chatSessionId ? this._sessionTerminalAssociations.get(context.chatSessionId)?.instance : undefined;
-		let toolEditedCommand: string | undefined = await this._rewriteCommandIfNeeded(args, instance, shell);
-		if (toolEditedCommand === args.command) {
-			toolEditedCommand = undefined;
-		}
-
 		let confirmationMessages: IToolConfirmationMessages | undefined;
 		if (alternativeRecommendation) {
 			confirmationMessages = undefined;
 		} else {
-			const actualCommand = toolEditedCommand ?? args.command;
-			const subCommands = splitCommandLineIntoSubCommands(actualCommand, shell, os);
+			const subCommands = splitCommandLineIntoSubCommands(args.command, shell, os);
 			const inlineSubCommands = subCommands.map(e => Array.from(extractInlineSubCommands(e, shell, os))).flat();
 			const allSubCommands = [...subCommands, ...inlineSubCommands];
 			const subCommandResults = allSubCommands.map(e => this._commandLineAutoApprover.isCommandAutoApproved(e, shell, os));
-			const commandLineResult = this._commandLineAutoApprover.isCommandLineAutoApproved(actualCommand);
+			const commandLineResult = this._commandLineAutoApprover.isCommandLineAutoApproved(args.command);
 			const autoApproveReasons: string[] = [
 				...subCommandResults.map(e => e.reason),
 				commandLineResult.reason,
@@ -258,6 +251,12 @@ export class RunInTerminalTool extends Disposable implements IToolImpl {
 				message: new MarkdownString(args.explanation),
 				disclaimer,
 			};
+		}
+
+		const instance = context.chatSessionId ? this._sessionTerminalAssociations.get(context.chatSessionId)?.instance : undefined;
+		let toolEditedCommand: string | undefined = await this._rewriteCommandIfNeeded(args, instance, shell);
+		if (toolEditedCommand === args.command) {
+			toolEditedCommand = undefined;
 		}
 
 		return {
